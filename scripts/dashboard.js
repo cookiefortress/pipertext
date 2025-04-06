cd7b040fcb5f5f3c4224a0997f49aaa19fb9c4b9
cookiefortress <iwantcookieplz@gmail.com>
2025-04-06 13:09:09 -0400

    Implement automatic sidebar insertion

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -17,6 +17,10 @@ function getUTC() {
 	return UTC;
 }
 
+// select the sidebar
+const sidebar = document.querySelector("aside");
+
+// create the dashboard with the time in UTC
 let dashboard = document.createElement("div");
 dashboard.style.color = "var(--light)";
 dashboard.style.backgroundColor = "black";
@@ -33,9 +37,56 @@ dashboard.style.alignItems = "center";
 dashboard.style.gap = ".5rem"
 dashboard.classList.add(".dashboard");
 
-const sidebar = document.querySelector("aside");
-sidebar.appendChild(dashboard);
+let mainMarquee = document.createElement("marquee");
+mainMarquee.innerHTML = `
+	<img src="/gif/bestvw.gif" alt="Best viewed with a computer and a monitor!">
+    <img src="/gif/fftake.gif" alt="Firefox - take back the web!">
+    <img src="/gif/css2.gif" alt="Made with CSS!">
+    <img src="/gif/gothtml.gif" alt="Got HTML?">`
 
 let pipertextIcon = "<img src='/img/pipertext.png' alt='Pipertext 88x31 icon' width='88px'>";
+
+let recentPosts = document.createElement("div");
+let recentPostsHeader = document.createElement("h3");
+recentPostsHeader.textContent = "Recent posts";
+recentPosts.appendChild(recentPostsHeader);
+
+let recentPost = document.createElement("div");
+recentPost.classList.add("recentArticle");
+
+let postLink = document.createElement("a");
+postLink.href = "/html/articles/microsoft50.html";
+postLink.textContent = "50 years of Microsoft";
+let postDate = document.createElement("p");
+postDate.textContent = "2025/04/06";
+
+recentPost.appendChild(postLink);
+recentPost.appendChild(postDate);
+recentPosts.appendChild(recentPost);
+
+postLink = document.createElement("a");
+postLink.href = "/html/articles/touchPipertext.html";
+postLink.textContent = "touch pipertext.ptml";
+postDate = document.createElement("p");
+postDate.textContent = "2025/04/06";
+
+recentPost.appendChild(postLink);
+recentPost.appendChild(postDate);
+recentPosts.appendChild(recentPost);
+
+postLink = document.createElement("a");
+postLink.href = "/html/articles/touchPipertext.html";
+postLink.textContent = "touch pipertext.ptml";
+postDate = document.createElement("p");
+postDate.textContent = "2025/04/06";
+
+recentPost.appendChild(postLink);
+recentPost.appendChild(postDate);
+recentPosts.appendChild(recentPost);
+
+// append the marque, dashboard, and recentPosts to the sidebar
+sidebar.appendChild(mainMarquee);
+sidebar.appendChild(dashboard);
+sidebar.appendChild(recentPosts);
 
 writeUTC();
