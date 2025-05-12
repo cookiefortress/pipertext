1b27586d89f09a064b751a3a920c52e914ef4930
cookiefortress <iwantcookieplz@gmail.com>
2025-05-11 23:14:08 -0400

    Make the typewriter script into an IIFE

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,41 +1,43 @@
-// select the h1 with class "pipertext"
-let header = document.querySelector(".pipertext");
+(function () {
+  // select the h1 with class "pipertext"
+  let header = document.querySelector(".pipertext");
 
-// makes pipertext invisible
-function makeInvis() {
-  header.style.visibility = "hidden";
-}
-// makes pipertext visible
-function makeVis() {
-  header.style.visibility = "visible";
-}
-// makes pipertext blink (invis, vis, invis etc.)
-function blink() {
-  for (let i = 0; i < 2; i++) {
-    setTimeout(() => makeInvis(), i * 600);
-    setTimeout(() => makeVis(), i * 600 + 300);
+  // makes pipertext invisible
+  function makeInvis() {
+    header.style.visibility = "hidden";
   }
-}
-
-async function typewriter(str) {
-  /* clears the header content because layout.js automatically inserts 
-	"> pipertext" into the header */
-  header.textContent = "";
-  // for each character of the string, add the character incrementally in spans of .2 seconds starting from the beginning
-  for (let i = 0; i < str.length; i++) {
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        header.textContent += str[i];
-        // this line makes the arrow bracket and a space following it appear at the same time
-        if (str[i] == ">") header.textContent += " ";
-        resolve();
-      }, 200);
-    });
+  // makes pipertext visible
+  function makeVis() {
+    header.style.visibility = "visible";
   }
-  setTimeout(() => {
-    blink();
-  }, 200);
-}
+  // makes pipertext blink (invis, vis, invis etc.)
+  function blink() {
+    for (let i = 0; i < 2; i++) {
+      setTimeout(() => makeInvis(), i * 600);
+      setTimeout(() => makeVis(), i * 600 + 300);
+    }
+  }
 
-const pipertext = ">pipertext";
-typewriter(pipertext);
+  async function typewriter(str) {
+    /* clears the header content because layout.js automatically inserts 
+    "> pipertext" into the header */
+    header.textContent = "";
+    // for each character of the string, add the character incrementally in spans of .2 seconds starting from the beginning
+    for (let i = 0; i < str.length; i++) {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          header.textContent += str[i];
+          // this line makes the arrow bracket and a space following it appear at the same time
+          if (str[i] == ">") header.textContent += " ";
+          resolve();
+        }, 200);
+      });
+    }
+    setTimeout(() => {
+      blink();
+    }, 200);
+  }
+
+  const pipertext = ">pipertext";
+  typewriter(pipertext);
+})();
